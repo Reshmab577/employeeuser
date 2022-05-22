a286f65b8fa76dafd156adae4f3cb65226e4608d
Reshmab577 <reshmaresh5799@gmail.com>
2022-05-22 19:43:46 +0530

    addnavbarpage

--- src/Components/Userregester.js
+++ src/Components/Userregester.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Userregester = () => {
   
@@ -17,9 +18,11 @@ const Userregester = () => {
 
       }
       return (
+          
 
 
     <div>
+        <Navbar/>
         <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
